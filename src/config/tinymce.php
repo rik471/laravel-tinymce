ec6e4b176f463f7557625a6eda9c09ae1b3c4802
ktquez <ktquez@gmail.com>
2016-03-31 08:31:48 -0300

    Updated version of TinyMCE and increase the configuration option to each textarea

--- src/config/tinymce.php
+++ src/config/tinymce.php
@@ -2,10 +2,25 @@
 
 return [
 
-	'cdn' => config('app.url').'/vendor/js/tinymce/tinymce.min.js',
+	'cdn' => url('vendor/js/tinymce/tinymce.min.js'),
 
-	'params' => [
-		"selector" => "#tinymce",
+	'default' => [
+		"selector" => ".tinymce",
+		"language" => 'en',
+		"theme" => "modern",
+		"skin" => "lightgray",
+		"plugins" => [
+	         "advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker",
+	         "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
+	         "save table contextmenu directionality emoticons template paste textcolor"
+		],
+		"toolbar" => "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | l      ink image | print preview media fullpage | forecolor backcolor emoticons",
+	],
+
+	// Custom
+	
+	/*'example' => [
+		"selector" => "#example",
 		"language" => 'pt_BR',
 		"theme" => "modern",
 		"skin" => "lightgray",
@@ -15,6 +30,6 @@ return [
 	         "save table contextmenu directionality emoticons template paste textcolor"
 		],
 		"toolbar" => "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | l      ink image | print preview media fullpage | forecolor backcolor emoticons",
-	]
+	],*/
 
 ];
